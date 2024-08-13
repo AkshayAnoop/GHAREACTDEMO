2e8d4f3f4c1efeb1ff501844663d51f260670362
AkshayAnoop <akshayanoop689@gmail.com>
2024-08-13 14:30:08 +0530

    just commits

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
+// import TaskData from './component/TaskData';
+// import ProjectCard from './component/ProjectCard';
+// import ProjectsList from './component/ProjectsList';
 // import TaskList from './component/TaskList';
-// import Demo from './component/Demo';
-import TaskData from './component/TaskData';
-import ProjectCard from './component/ProjectCard';
-import ProjectsList from './component/ProjectsList';
-import TaskList from './component/TaskList';
 import LoginStatus from './component/Login';  
-
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-        {/* <TaskList /> */}
-        {/* <TaskData/> */}
-        <LoginStatus/>
+      {/* Uncomment and use the components when needed */}
+      {/* <TaskList /> */}
+      {/* <TaskData /> */}
+      {/* <ProjectCard /> */}
+      {/* <ProjectsList /> */}
+      <LoginStatus />
     </div>
   );
 }
